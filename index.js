49ecedbc796f90b06961b5f43766652bada74b6e
riquee <strikedv@hotmail.com>
2021-08-13 11:11:21 -0300

    create end-point

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const meli = require('mercadolibre-nodejs-sdk');
+const port = 3000;
+require('dotenv/config');
+
+app.use(express.json());
+
+app.use('/', express.static(`${__dirname}/public`));
+
+app.get('/callback', async (req, res) => {
+  const { code } = req.query;
+
+  const apiInstance = new meli.OAuth20Api();
+  const opts = {
+    grantType: 'authorization_code',
+    clientId: process.env.MELI_CLIENT_ID,
+    clientSecret: process.env.MELI_CLIENT_SECRET,
+    redirectUri: process.env.MELI_CLIENT_URI,
+    code,
+    refreshToken: process.env.MELI_CLIENT_URI,
+  };
+  apiInstance.getToken(opts, (error, data, response) => {
+    if (error) {
+      return res.status(200).send(error);
+    } else {
+      const [accessToken] = response.text.match(/(APP_USR-)(\w+-){3}\d+/);
+      return res.status(200).send({ token: accessToken, code });
+    }
+  });
+});
+
+app.listen(port, () => console.log(`CONNECT PORT: 3000`));
