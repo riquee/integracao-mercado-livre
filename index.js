7ed52b99af73f61ca7110a920bf028ec3ff4a973
riquee <strikedv@hotmail.com>
2021-08-13 11:40:39 -0300

    eae

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
 const express = require('express');
 const app = express();
-const meli = require('mercadolibre-nodejs-sdk');
-require('dotenv/config');
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'ok' });
-});
-
-app.get('/callback', async (req, res) => {
-  const { code } = req.query;
-
-  const apiInstance = new meli.OAuth20Api();
-  const opts = {
-    grantType: 'authorization_code',
-    clientId: process.env.MELI_CLIENT_ID,
-    clientSecret: process.env.MELI_CLIENT_SECRET,
-    redirectUri: process.env.MELI_CLIENT_URI,
-    code,
-    refreshToken: process.env.MELI_CLIENT_URI,
-  };
-  apiInstance.getToken(opts, (error, data, response) => {
-    if (error) {
-      return res.status(200).send(error);
-    } else {
-      const [accessToken] = response.text.match(/(APP_USR-)(\w+-){3}\d+/);
-      return res.status(200).send({ token: accessToken, code });
-    }
-  });
-});
-
-app.listen(PORT, () => console.log(`CONNECT PORT: 3000`));
+app.get('/', (req, res) => res.send('Está vivo!!!'));
+app.listen(port, () => console.log(`Ouvind  o na porta ${port}`));
